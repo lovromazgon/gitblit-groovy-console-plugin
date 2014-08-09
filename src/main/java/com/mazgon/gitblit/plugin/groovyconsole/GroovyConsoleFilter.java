8d21f829e442c12ac18dd1398acdbe29ccb33ac1
lovro <lovro.mazgon@gmail.com>
2014-08-09 10:15:06 +0200

    make request via AJAX

--- src/main/java/com/mazgon/gitblit/plugin/groovyconsole/GroovyConsoleFilter.java
+++ src/main/java/com/mazgon/gitblit/plugin/groovyconsole/GroovyConsoleFilter.java
@@ -13,15 +13,22 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 /**
  * @author lovro.mazgon
  */
 @Extension
 public class GroovyConsoleFilter extends HttpRequestFilter {
+	private static final String CODE_REQUEST_PARAM = "code";
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        if (!(request instanceof HttpServletRequest) || !(response instanceof HttpServletResponse)) {
+		System.out.println("IN FILTER");
+        if (!(request instanceof HttpServletRequest)
+				|| !(response instanceof HttpServletResponse)
+				|| !isRequestToGroovyConsole((HttpServletRequest) request)) {
             chain.doFilter(request, response);
             return;
         }
@@ -29,24 +36,38 @@ public class GroovyConsoleFilter extends HttpRequestFilter {
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         HttpServletRequest httpRequest = (HttpServletRequest) request;
 
-        if (canAccessGroovyConsole(httpRequest)) {
-            chain.doFilter(request, response);
-        } else {
-            httpResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden access");
-        }
+		if (!canAccessGroovyConsole(httpRequest)) {
+			httpResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden access");
+			return;
+		}
+
+		if (httpRequest.getMethod().equalsIgnoreCase("post")) {
+			String output;
+			String groovyCode = httpRequest.getParameter(CODE_REQUEST_PARAM);
+			try {
+				output = Plugin.instance().getGroovyConsole().executeGroovyScript(groovyCode);
+			} catch (Exception e) {
+				StringWriter errors = new StringWriter();
+				e.printStackTrace(new PrintWriter(errors));
+				output = errors.toString();
+			}
+			httpResponse.getWriter().print(output);
+		}
+		else {
+			chain.doFilter(request, response);
+		}
     }
 
+	private boolean isRequestToGroovyConsole(HttpServletRequest httpRequest) {
+		return httpRequest.getRequestURI().startsWith(Plugin.instance().getGroovyConsolePath());
+	}
 
     private boolean canAccessGroovyConsole(HttpServletRequest httpRequest) {
-        if (httpRequest.getRequestURI().equals(Plugin.instance().getGroovyConsolePath())) {
-            // request is for the Groovy Console, authorize admins
-            IAuthenticationManager authManager = GitblitContext.getManager(IAuthenticationManager.class);
-            UserModel user = authManager.authenticate(httpRequest);
-            if (user == null) {
-                return false;
-            }
-            return user.canAdmin();
-        }
-        return true;
+		IAuthenticationManager authManager = GitblitContext.getManager(IAuthenticationManager.class);
+		UserModel user = authManager.authenticate(httpRequest);
+		if (user == null) {
+			return false;
+		}
+		return user.canAdmin();
     }
 }
--- src/main/java/com/mazgon/gitblit/plugin/groovyconsole/GroovyConsoleWebPage.java
+++ src/main/java/com/mazgon/gitblit/plugin/groovyconsole/GroovyConsoleWebPage.java
@@ -1,41 +1,9 @@
 package com.mazgon.gitblit.plugin.groovyconsole;
 
-import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.protocol.http.WebRequest;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 
 /**
  * @author lovro.mazgon
  */
 public class GroovyConsoleWebPage extends WebPage {
-    private static final String CODE_REQUEST_PARAM = "code";
-    private static final String OUTPUT_LABEL_NAME = "output";
-    private static final String GROOVY_CODE_LABEL_NAME = "groovycode";
-
-    public GroovyConsoleWebPage(PageParameters parameters) {
-        super(parameters);
-
-        String groovyCode = "";
-        String output = "";
-
-        final WebRequest wr = (WebRequest) getRequest();
-        final HttpServletRequest servletRequest = wr.getHttpServletRequest();
-        if (servletRequest.getMethod().equalsIgnoreCase("post")) {
-            groovyCode = servletRequest.getParameter(CODE_REQUEST_PARAM);
-            try {
-                output = Plugin.instance().getGroovyConsole().executeGroovyScript(groovyCode);
-            } catch (Exception e) {
-                StringWriter errors = new StringWriter();
-                e.printStackTrace(new PrintWriter(errors));
-                output = errors.toString();
-            }
-        }
-        add(new Label(OUTPUT_LABEL_NAME, output));
-        add(new Label(GROOVY_CODE_LABEL_NAME, groovyCode));
-    }
 }
